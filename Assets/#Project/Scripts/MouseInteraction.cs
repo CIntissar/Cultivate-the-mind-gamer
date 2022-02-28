2caff60b9f2f620ed1595b3b2a7c77602f7d99f0
Yasmine El Messaoudi <yasnardam@hotmail.com>
2022-02-28 17:53:23 +0100

    MouseOver works

--- Assets/#Project/Scripts/MouseInteraction.cs
+++ Assets/#Project/Scripts/MouseInteraction.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MouseInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void OnMouseOver()
     {
-        
+        //cursor change
+        //Debug.Log("hovering !");
+    }
+    void OnMouseExit()
+    {
+        //Debug.Log("not hovering...");
+    }
+    void OnClick()
+    {
+        if(gameObject.CompareTag("Ball"))
+        {
+            //do something
+            Debug.Log("Clicked");
+        }
+    }
+    void RollOver()
+    {
+
     }
 }

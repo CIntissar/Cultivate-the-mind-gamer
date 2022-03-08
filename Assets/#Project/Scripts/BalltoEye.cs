bd1cff33298c6a0fdb3a4f2bd4293b941b567d76
CIntissar <intissar.chajaadine@hotmail.com>
2022-03-08 17:24:50 +0100

    That's all folks... for today

--- Assets/#Project/Scripts/BalltoEye.cs
+++ Assets/#Project/Scripts/BalltoEye.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BalltoEye : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Function to make the pupil float after one click
+    
+    //make it change as an apple? -> having other paints reference?
+    public float clickCount = 0;
 
-    // Update is called once per frame
+
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(clickCount == 1)
+            {
+                FloatingBall();
+            }
+            else if(clickCount == 2)
+            {
+                Blink();
+            }
+        }
+    }
+
+    void FloatingBall()
+    {
+        transform.DOMoveY(transform.position.y + 5.0f, 2f);
+    }
+
+    void Blink()
+    {
+        //having a transition (fading in/out) to make appear the eyes 
+        // animation blink every 5 sec?
         
     }
+    
 }

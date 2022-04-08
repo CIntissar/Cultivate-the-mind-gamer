f940b67a13313d7b3f0ea65a11b29d79a47c405b
CIntissar <intissar.chajaadine@hotmail.com>
2022-04-08 13:50:35 +0200

    You can open it now

--- Assets/#Project/Scripts/TestScirpt.cs
+++ Assets/#Project/Scripts/TestScirpt.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 
-public class TestScirpt : MonoBehaviour, IPointerClickHandler
+public class TestScirpt : MonoBehaviour // IPointerClickHandler
 {
+    public int count = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        count++;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print(count);
     }
 }
